e9cbed9441fa876e6ab0a9c23c99438abb2da135
szywind <zhenyuan@cs.wisc.edu>
2017-11-03 18:53:13 +0800

    finish collaborative filtering

--- code/ClothesRecommend.py
+++ code/ClothesRecommend.py
@@ -9,7 +9,7 @@ from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
 
 os.environ['PYSPARK_PYTHON'] = sys.executable
 
-local_test = True
+local_test = False
 
 if local_test:
     HDFS_ROOT = "hdfs://localhost:9000/demo/" # set by conf of hadoop
@@ -216,13 +216,13 @@ if __name__ == "__main__":
 
 
 '''
-import os, sys, random
-import pandas as pd
+import os, sys
 from pyspark import mllib
 from pyspark import SparkContext, SparkConf
+import numpy as np
+from numpy.linalg import norm
+
 from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
-
-
 conf = (SparkConf().set("spark.master", "spark://localhost:7077")  
         .set("spark.eventLog.enabled)",True)
         .set("spark.task.cpus",1)
@@ -255,7 +255,120 @@ print("Mean Squared Error = " + str(MSE))
 # Save and load model
 model_path = HDFS_ROOT + 'models/modelCF'
 
-model.save(sc, model_path)
-sameModel = MatrixFactorizationModel.load(sc, model_path)
+# model.save(sc, model_path)
+# sameModel = MatrixFactorizationModel.load(sc, model_path)
+
+
+
+
+
+
+def extract_user_id(uid):
+    return int(uid[:uid.rfind('.')])
+
+def extract_item_id(cid):
+    return int(cid[cid.find('_') + 1:cid.rfind('.')])
+
+def cosin_similarity(u, v):
+    return (np.inner(u, v)/(norm(u)*norm(v)) + 1) / 2.0
+
+rating_data = sc.textFile(HDFS_ROOT + 'input/ratings')
+item_data = sc.textFile(HDFS_ROOT + 'input/clothes-features.csv')
+user_data = sc.textFile(HDFS_ROOT + 'input/user-features.csv')
+
+ratings = rating_data.map(lambda l: l.split(',')).map(lambda l: Rating(int(l[0]), int(l[1]), float(l[2])))
+items = item_data.map(lambda l: l.split(',')).map(lambda l: (extract_item_id(l[0]), list(map(float, l[1:]))))
+users = user_data.map(lambda l: l.split(',')).map(lambda l: (extract_user_id(l[-1]), list(map(float, l[:-1]))))
+
+num_users = users.count()
+num_items = items.count()
+
+# get {user: {item: rating}}
+user_rating_dict = dict(ratings.map(lambda l: (int(l[0]), (int(l[1]), float(l[2])))).groupByKey().mapValues(dict).collect())
+# get {item: {user: rating}}
+item_rating_dict = dict(ratings.map(lambda l: (int(l[1]), (int(l[0]), float(l[2])))).groupByKey().mapValues(dict).collect())
+
+# https://stackoverflow.com/questions/40087483/spark-average-of-values-instead-of-sum-in-reducebykey-using-scala
+# compute average rating of each item
+item_avg_rating = ratings.map(lambda l: (int(l[1]), (float(l[2]), 1))).\
+            reduceByKey(lambda x,y: (x[0]+y[0], x[1]+y[1])).mapValues(lambda x: 1.0 * x[0] / x[1]).collectAsMap()
+
+
+
+def predModelCF(uid, cid):
+    if model is None:
+        try:
+            model_path = HDFS_ROOT + 'models/modelCF'
+            model = MatrixFactorizationModel.load(sc, model_path)
+        except:
+            raise RuntimeError("No model found!")
+    rating = model.predict(uid, cid)
+    return rating
+
+
+def predUserCF(uid, cid):
+    rating = 0
+    denominator = 0.0
+    if uid in user_rating_dict:
+        if cid in user_rating_dict[uid]:
+            return user_rating_dict[uid][cid]
+    for u in user_rating_dict:
+        if u == uid:
+            continue
+        try:
+            rating += user_rating_dict[u][cid] * user_similarity[(uid, u)]
+            denominator += user_similarity[(uid, u)]
+        except:
+            pass
+    if denominator == 0:
+        try:
+            rating = item_avg_rating[cid]
+        except:
+            pass
+    else:
+        rating /= denominator
+    return rating
+
+def predItemCF(uid, cid):
+    rating = 0
+    denominator = 0.0
+    if uid in user_rating_dict:
+        if cid in user_rating_dict[uid]:
+            return user_rating_dict[uid][cid]
+        for c in user_rating_dict[uid]:
+            try:
+                rating += user_rating_dict[uid][c] * item_similarity[(cid, c)]
+                denominator += item_similarity[(cid, c)]
+            except:
+                pass
+    if denominator == 0:
+        try:
+            rating = item_avg_rating[cid]
+        except:
+            pass
+    else:
+        rating /= denominator
+    return rating
+
+
+
+# compute similarity b/w users
+user_list = users.collect()
+user_similarity = {}
+for i in range(num_users):
+    for j in range(i, num_users):
+        u = user_list[i]
+        v = user_list[j]
+        user_similarity[(u[0], v[0])] = user_similarity[(v[0], u[0])] = cosin_similarity(u[1], v[1])
+
+# compute similarity b/w items
+item_list = items.collect()
+item_similarity = {}
+for i in range(num_items):
+    for j in range(i, num_items):
+        u = item_list[i]
+        v = item_list[j]
+        item_similarity[(u[0], v[0])] = item_similarity[(v[0], u[0])] = cosin_similarity(u[1], v[1])
+
 
 '''
